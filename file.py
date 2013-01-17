c53867b2c9009b4a7477aa7d3bdea1a550bc7962
perfectmak <damiperfect@gmail.com>
2013-01-17 19:04:53 +0000

    Update file.py

--- file.py
+++ file.py
@@ -1,15 +1,27 @@
+#This class is supposed to encapsulate an instance of a file.
+#
+#It should contain methods to manipulate File Information
+#and also retrieve file information.
+#
+#It is mean't to be a helper class to the Database Class
+#when storing information.
+
+import os
+
 class File:
-    def __init__(self, item):
-        self._name = getName(item)
-        self._path = getPath(item)
-        self._size = getSize(item)
-        self._datec = getDatec(item)
-        self._datem = getDatem(item)
-        self._extension = getExtension(item)
+    def __init__(self, item):           #constructor takes a file's full directory address
+        self._name = self.getName(item)
+        self._path = self.getPath(item)
+        self._size = self.getSize(item)
+        self._datec = self.getDatec(item)
+        self._datem = self.getDatem(item)
+        self._extension = self.getExtension(item)
 
-    def getDetails(self):
-        return (self._name, self._path, self._size, self._datec, self._datem, self._extension)
+    def getDetails(self):   #returns tuple containing all relevant information about the file
+        return (None, self._name, self._path, self._size, self._datec, self._datem, self._extension)
 
+    #The get functions help get the corresponding properties,
+    # e.g getName() returns the name of the File.
     def getName(self, item):
         return os.path.basename(item)
 
