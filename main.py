919bc91d92baeb2449760c39f6f3a80ab69eb2ff
perfectmak <damiperfect@gmail.com>
2013-01-17 11:10:40 -0800

    Create main.py
    The main entry point for the application

---
+++ main.py
@@ -0,0 +1,11 @@
+#The main entrance into the File system Application
+
+#Should contain all the visible workings of Program
+
+import framework
+
+if __name__ == '__main__':
+
+    test = framework.Framework()
+    
+    test.showAll();
